938c85a0dd26fa4cd58001fca740aedf5e0ca71b
Tiago Olimpio <to.olimpio@gmail.com>
2020-03-06 03:49:12 -0300

    Adicionando novos post ao blog

--- src/main/java/br/com/to/codeblog/controller/CodeBlogController.java
+++ src/main/java/br/com/to/codeblog/controller/CodeBlogController.java
@@ -1,13 +1,18 @@
 package br.com.to.codeblog.controller;
 
+import java.time.LocalDate;
 import java.util.List;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.to.codeblog.model.Post;
 import br.com.to.codeblog.service.CodeBlogService;
@@ -34,4 +39,18 @@ public class CodeBlogController {
 		return mv;
 	}
 	
+	@RequestMapping(value = "/newpost", method = RequestMethod.GET)
+	public String getPostForm() {
+		return "postForm";
+	}
+	
+	@RequestMapping(value = "/newpost", method = RequestMethod.POST)
+	public String savePost(@Valid Post post, BindingResult result, RedirectAttributes attributes) {
+		if (result.hasErrors()) {
+			return "redirect:/newpost";
+		}
+		post.setData(LocalDate.now());
+		codeBlogService.save(post);
+		return "redirect:/posts";
+	}
 }
